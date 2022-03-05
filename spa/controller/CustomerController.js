8f8a4f280cbd284ed7811c83485ee4f8e3c2a15b
vidushalakshan <vidushalakshan7890@gmail.com>
2022-03-05 15:00:47 +0530

    updated customer save content

---
+++ spa/controller/CustomerController.js
@@ -0,0 +1,33 @@
+/*customer save*/
+
+function CustomerSave() {
+    let id=$("#inputI").val( );
+    let name=$("#inputNam").val();
+    let address=$("#inputAddres").val();
+    let salary=$("#inputSalar").val();
+
+    var CustomerObject={
+        id:id,
+        name:name,
+        address:address,
+        salary:salary
+    };
+
+    customerTable.push(CustomerObject);
+
+}
+
+function loadAllCustomer() {
+    $("#tblCustomer").empty();
+    for (var i of customerTable) {
+        let row = `<tr><td>${i.id}</td><td>${i.name}</td><td>${i.address}</td><td>${i.salary}</td></tr>`;
+        $("#tblCustomer").append(row);
+    }
+}
+
+$("#btnCustomerSave").click(function () {
+    CustomerSave();
+    loadAllCustomer();
+});
+
+
