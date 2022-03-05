d064b8aafa5019affd4ea5f54d74d3b5f4249005
vidushalakshan <vidushalakshan7890@gmail.com>
2022-03-05 09:35:29 +0530

    added model item contetn

---
+++ spa/model/Item.js
@@ -0,0 +1,34 @@
+function Item(code,name,qty,price) {
+    let __code=code;
+    let __name=name;
+    let __qty=qty;
+    let __price=price;
+
+    this.getCode=function () {
+        return __code;
+    }
+    this.setCode=function (code) {
+        __code=code;
+    }
+
+    this.getName=function () {
+        return name;
+    }
+    this.setName=function (name) {
+        __name=name;
+    }
+
+    this.getQty=function () {
+        __qty=qty;
+    }
+    this.setQty=function (qty) {
+        __qty=qty;
+    }
+
+    this.getPrice=function () {
+        return price;
+    }
+    this.setPrice=function (price) {
+        __price=price;
+    }
+}
